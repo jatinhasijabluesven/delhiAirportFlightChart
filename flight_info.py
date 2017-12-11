b5550d3e603ca8a5e0da645f9e0da0b1c2dcde38
jatinhasijabluesven <34453887+jatinhasijabluesven@users.noreply.github.com>
2017-12-11 22:32:05 +0000

    Added inline comments

--- flight_info.py
+++ flight_info.py
@@ -10,6 +10,7 @@ url_arrival="https://api.laminardata.aero/v1/aerodromes/VIDP/arrivals?user_key=9
 
 def parseFlighStatData(url,data_file, verbose, chosen_info_type):
     
+    # try retrieving live flight data from API first, otherwise use sample json files.
     response=requests.get(url,headers={"accept":"application/json"})
     flight_data=None
     
@@ -20,11 +21,13 @@ def parseFlighStatData(url,data_file, verbose, chosen_info_type):
         print("API does not work, parsing from sample file instead")
         with open(data_file) as file:
             flight_data = json.load(file)
-        
+    
+    #stores the data of flights arriving or departing betweeb 1200 and 1800 IST, keyed by date     
     parsed_data={}
     if(flight_data!=None):
         for item in flight_data['features']:
             try:
+                #flight arrival or departure time
                 time = item['properties'][chosen_info_type.name]['runwayTime']['initial']
             except KeyError:
                 continue
@@ -33,6 +36,7 @@ def parseFlighStatData(url,data_file, verbose, chosen_info_type):
             hour = datetime.strptime(time, "%Y-%m-%d %H:%M:%S").hour
             minute = datetime.strptime(time, "%Y-%m-%d %H:%M:%S").minute
             
+            #API uses UTC time, therefore between 0630 and 1230 UTC (IST=UTC+530)
             if( ((hour==6 and minute>=30) or (hour >= 7)) and ((hour < 12) or (hour == 12 and minute <= 29)) ):
                 if(day not in parsed_data):
                     parsed_data[day]=[]
@@ -45,10 +49,12 @@ def parseFlighStatData(url,data_file, verbose, chosen_info_type):
                 parsed_data[day].append({ 'arrival':arrival , 'departure':departure,  
                                        'airline':airline, 'time':time})
         
+        #print the number of flights between out interval keyed by date
         for key in parsed_data.keys():
             num=len(parsed_data[key])
             print("No. of flight {0} or scehdule on {1} Dec on delhi airport in our interval is {2}".format(chosen_info_type.name,key,num))
-            
+        
+        #print the flight info as well
         if(verbose):
             for key in parsed_data.keys():
                 num=len(parsed_data[key])
@@ -61,7 +67,7 @@ def getCommandLineParser():
     parser = argparse.ArgumentParser(description='FlightInfo')
     parser.add_argument('--t',required=False,default="arrival",
         help='--t arrival for displaying arrival, --t departure for displaying departure')
-    parser.add_argument('--v',required=False,default="false",
+    parser.add_argument('--v',required=False,default="true",
         help='--v true for detailed flight output, false for just count')
     return parser
 
@@ -71,8 +77,12 @@ def printDelhiArrival(verbose):
 def printDelhiDeparture(verbose):
     parseFlighStatData(url_departure,"data_departure.json",verbose,info_type.departure)
 
+
 if __name__ == '__main__':
-    
+
+    # Command Line Argumnent
+    #    --t arrival for displaying flight arrivals, --t departure for displaying fligh departures (default=arrival)
+    #    --v true for displaying flight info, false for just displaying counts (default= true)
     parser=getCommandLineParser()
     args = parser.parse_args()
     
